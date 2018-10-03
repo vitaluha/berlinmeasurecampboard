c71d8b3632626d9aed4a6e79bcf54947071c7ee7
Vitaliy Matiyash <vitaliy.matiyash@alliancedata.com>
2018-10-03 09:34:24 -0400

    Initial commit for Berlin MeasureCamp

---
+++ webroot/board-config.js
@@ -0,0 +1,8 @@
+gOptions = {
+  // This can have nested stuff, arrays, etc.
+  enabled: true,
+  city: 'Berlin',
+  venue_name: '',
+  venue_address: '',
+  google_sheet_url: 'https://docs.google.com/spreadsheets/d/18CVtlVsdTvn8kxJLCKcROH3lv_iO1hpIimWZzMfMZjE/edit?usp=sharing'
+}
